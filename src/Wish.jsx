58c42dc8a64f4490d8fa47c289a25a38fd5ef6ad
Zakaria <zakariaakhmad@gmail.com>
2022-08-22 11:13:13 +0700

    test wish page

--- src/Wish.jsx
+++ src/Wish.jsx
@@ -7,7 +7,7 @@ const Wish = ({ name }) => {
         HAPPY BIRTHDAY <span className='highlight'>{name.toUpperCase()}</span> !!!
       </h1>
       <div className='wish-detail'>
-      <br /><br />Most things in life dont come easy; tough decission, make smart choices -- being prepared is the only way to know to ensure that we are going to win. <br /><br />But the most important things that we should remember is; we are not living in fear, we are living in faith. <br /><br />Keep the faith hold on, things will get better. It might be stormy now, but there is golden sky await. <br /><br />Happy birthday to the person i love the most. Hope all your wishes come true. <br /><br />For now, lets hold my hand and facing this world together - WE RIDE! :D
+      <br />Most things in life dont come easy; tough decission, make smart choices -- being prepared is the only way to know to ensure that we are going to win. <br /><br />But the most important things that we should remember is; we are not living in fear, we are living in faith. <br /><br />Keep the faith hold on, things will get better. It might be stormy now, but there is golden sky await. <br /><br />Happy birthday to the person i love the most. Hope all your wishes come true. <br /><br />♥, Zak
       </div>
     </div>
   );
